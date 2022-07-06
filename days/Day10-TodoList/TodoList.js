09f452cd05006b488c6c3595095127de6d23ed2d
Phong Tran <phong.tranthanh@2359media.com>
2022-07-06 17:26:36 +0700

    day 10

---
+++ days/Day10-TodoList/TodoList.js
@@ -0,0 +1,56 @@
+var input = document.querySelector("input");
+var butotn = document.querySelector("button");
+var form = document.querySelector("form");
+var todos = document.querySelector(".todos");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let value = input.value.trim();
+  if (value) {
+    addTodoElement({ text: value });
+    saveTodoList();
+  }
+  input.value = "";
+});
+
+function addTodoElement(todo) {
+  var li = document.createElement("li");
+  li.innerHTML = `
+  <span>${todo.text}</span>
+  <i class="fa-solid fa-trash-can"></i>
+`;
+  if (todo.status === "completed") {
+    li.setAttribute("class", "completed");
+  }
+  li.addEventListener("click", function () {
+    this.classList.toggle("completed");
+    saveTodoList();
+  });
+  li.querySelector("i").addEventListener("click", function () {
+    this.parentElement.remove();
+    saveTodoList();
+  });
+  todos.appendChild(li);
+}
+
+function saveTodoList() {
+  let todoList = document.querySelectorAll("li");
+  let todoStorage = [];
+  todoList.forEach(function (item) {
+    let text = item.querySelector("span").innerText;
+    let status = item.getAttribute("class");
+    console.log(status);
+
+    todoStorage.push({ text, status });
+  });
+  localStorage.setItem("todoList", JSON.stringify(todoStorage));
+}
+
+function init() {
+  let data = JSON.parse(localStorage.getItem("todoList"));
+  if (data == null) return;
+  data.forEach(function (item) {
+    addTodoElement(item);
+  });
+}
+init();
