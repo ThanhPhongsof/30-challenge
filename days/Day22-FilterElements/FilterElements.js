a935fcbc6981bd29c73a69779623b50047229897
ThanhPhongsof <ttphongletter@gmail.com>
2022-07-18 14:38:34 +0700

    day 22

---
+++ days/Day22-FilterElements/FilterElements.js
@@ -0,0 +1,19 @@
+const foodButtons = document.querySelectorAll(".food-menu button");
+const foodList = document.querySelectorAll(".food-item");
+
+[...foodButtons].forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const type = e.target.getAttribute("type-food");
+
+    document
+      .querySelector(".food-menu button.active")
+      .classList.remove("active");
+    e.target.classList.add("active");
+
+    foodList.forEach((item) => {
+      if (type == "all" || item.getAttribute("type-food") == type) {
+        item.classList.remove("hide");
+      } else item.classList.add("hide");
+    });
+  });
+});
