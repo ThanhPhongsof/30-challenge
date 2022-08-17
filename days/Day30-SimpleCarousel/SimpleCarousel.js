6efc0913e8cdf1773f9aa0315bd7b51ad41d98df
ThanhPhongsof <ttphongletter@gmail.com>
2022-08-17 11:01:30 +0700

    day 30

---
+++ days/Day30-SimpleCarousel/SimpleCarousel.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector(".wrapper");
+let isMouseDown = false;
+let startX, scrollLeft;
+
+wrapper.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+  startX = e.pageX - wrapper.offsetLeft;
+  scrollLeft = wrapper.scrollLeft;
+});
+
+wrapper.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+wrapper.addEventListener("mousemove", (e) => {
+  if (!isMouseDown) return;
+  const x = e.pageX - wrapper.offsetLeft;
+  // 3 la toc do srcoll
+  const walk = (x - startX) * 3;
+  wrapper.scrollLeft = scrollLeft - walk;
+});
